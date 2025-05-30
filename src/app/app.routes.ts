e50061039bd70e58fc745d54228113b9b4843eea
Liryel <liryelaguiar57@gmail.com>
2025-05-30 13:38:45 -0300

    Adição de ação no botão e modal de confirmação do login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
 
 export const routes: Routes = [
   {
-    path: "login",
-    component: LoginComponent
+    path: "",
+    component: DefaultLoginLayoutComponent
   }
 ];
--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-default-login-layout',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './default-login-layout.component.html',
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
+  mostrarModal: boolean = false
 
+  loginFeito() {
+    this.mostrarModal = !this.mostrarModal
+  }
 }
